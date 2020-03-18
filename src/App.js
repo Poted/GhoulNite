d4b8f78fcb7363d18fac471996d870c703851314
Poted <o.jpkmc@gmail.com>
2020-03-18 17:34:51 +0100

    fc

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import tweet from "./tweet.js";
+import Tweet from './tweet.js';
+
+function App() {
+  return (
+    <div className="app">
+       <Tweet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tweet.js
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./App.js";
+
+
+function Tweet() {
+    return (
+        <div className="tweet">
+            <h3>Name</h3>
+            <p>This is random tweet</p>
+            <h4>Number of likes</h4>
+
+        </div>
+    )
+}
+
+export default Tweet;
